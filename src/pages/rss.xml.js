aff4786258ee2027a45789d6257692ab03f4488f
akshayamaram <akshayamaramjesus@gmail.com>
2025-10-04 21:26:39 +0530

    learn: to use astros interactive islands to bring frontend framework components into our site, here used astro preact and use client directives and hydrated components

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -9,3 +9,11 @@ export async function GET(context) {
     customData: `<language>en-us</language>`,
   });
 }
+
+// This Astros official RSS package generates a non-HTML document with information about all of your blog posts that can be read by feed readers like Feedly, The Old Reader, and more. This document is updated every time your site is rebuilt.
+
+// Individuals can subscribe to your feed in a feed reader, and receive a notification when you publish a new blog post on your site, making it a popular blog feature.
+
+// View your RSS feed in a reader
+
+// Download a feed reader, or sign up for an online feed reader service and subscribe to your site by adding your own Netlify URL. You can also share this link with others so they can subscribe to your posts, and be notified when a new one is published.
